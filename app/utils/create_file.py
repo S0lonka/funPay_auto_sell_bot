2a8cce1cf2cbe877288b98c9c47285345742adef
S0lonka <solowayplay@gmail.com>
2025-07-29 09:51:23 +0300

    добавил функцию уведомлений win10toast -> ToastNotifier

--- app/utils/create_file.py
+++ app/utils/create_file.py
@@ -1,7 +1,7 @@
 def create_file(filepath: str, line_content: list[str]) -> None:
     """Создаёт и записывает файл
     Args:
-        filepath (str)           : Путь до целевого файла
+        filepath     (str)       : Путь до целевого файла
         line_content (list[str]) : Массив строк которыми заполнится файл
     
     Return:
--- app/utils/notification.py
+++ app/utils/notification.py
@@ -1,2 +1,42 @@
 from win10toast import ToastNotifier
 
+import logging
+import os
+
+from app.config.config import NOTIFICATION_FLAG
+
+
+# Настройка логгера
+logging.basicConfig(
+    level=logging.INFO,  # Уровень логирования (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(name)s - | %(levelname)s | -> %(message)s',
+    handlers=[
+        logging.FileHandler('funPay.log'),  # Логи в файл
+        logging.StreamHandler()             # Логи в консоль
+    ]
+)
+logger = logging.getLogger('notification')
+
+
+class Notification():
+    """Класс отправки уведомлений windows"""
+    def __init__(self):
+        self.notification_flag = NOTIFICATION_FLAG  # Флаг уведомлений из конфига
+        self.toaster = ToastNotifier()              # Модель класса уведомлений
+        self.icon_path = fr"{os.getcwd()}\app\img\icon\funPay.ico"                 # Путь до иконки
+
+
+    def send_notification(self, title: str, msg: str, duration: int = 10):
+        if NOTIFICATION_FLAG:
+            try:
+                self.toaster.show_toast(
+                    title=title,
+                    msg=msg,
+                    duration=duration,        # Длительность в секундах
+                    icon_path=self.icon_path, # Путь к иконке (опционально)
+                    threaded=True             # Показ уведомления в другом потоке (асинхронно)
+                )
+            except Exception as e:
+                logging.error(f"Ошибка уведомлений: {e}")
+        else:
+            logging.warning(f"Уведмления отключены: уведомление {title} не отправлено")
